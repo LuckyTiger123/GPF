27c74edd9e7246b646a7934bcde02bd2a4b2f1e3
Mercer <72289283+HeyMercer@users.noreply.github.com>
2023-10-16 12:59:36 +0800

    Add files via upload

---
+++ bio/run_gpf_plus_fewshot.sh
@@ -0,0 +1,8 @@
+#### GIN fine-tuning
+split=scaffold
+dataset=$1
+
+for runseed in 0 1 2 3 4
+do
+python prompt_tuning_full_shot.py --input_model_file models_graphcl/graphcl_80.pth --split $split --runseed $runseed --gnn_type gin --dataset $dataset --lr 1e-4 --dropout_ratio 0 --epochs 50 --tuning_type gpf-plus --num_layers 4 --pnum 20    
+done
